6aa19ea3adcec0bffd51f72fd587abbb2a930c1b
redplakad <redplakad@gmail.com>
2025-02-07 00:45:37 +0700

    Initiall commit

---
+++ resources/views/partials/styles.blade.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" type="text/css" href="{{ url('/assets/css/sidebar-menu.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ url('/assets/css/simplebar.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ url('/assets/css/apexcharts.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ url('/assets/css/prism.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ url('/assets/css/rangeslider.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ url('/assets/css/quill.snow.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ url('/assets/css/google-icon.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ url('/assets/css/remixicon.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ url('/assets/css/swiper-bundle.min.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ url('/assets/css/fullcalendar.main.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ url('/assets/css/jsvectormap.min.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ url('/assets/css/lightpick.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ url('/assets/scss/style.css') }}" />
+<link rel="icon" type="image/png" href="{{ url('/assets/images/favicon.png') }}">
